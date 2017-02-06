a865aeaca0ec15477bad5c7ff5800e972ca0f092
AliceThomas77 <alicethomas77@hotmail.co.uk>
2017-02-06 15:53:29 +0000

    First draft

---
+++ BookShop/Book.java
@@ -0,0 +1,20 @@
+package BookShop;
+
+public class Book {
+	
+	public String title;
+	
+	public String author;
+	
+	public String isbn;
+	
+	public String genre;	
+	
+	public Book(String title, String author, String isbn, String genre){
+		this.title = title;
+		this.author = author;
+		this.isbn = isbn;
+		this.genre = genre;
+	}
+
+}
